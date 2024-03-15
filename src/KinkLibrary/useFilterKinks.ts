fd89cceecb0b3805b3c0ff9f280f74e8521bfea9
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2024-03-15 08:37:58 -0400

    Fix favorite priority

--- src/KinkLibrary/useFilterKinks.ts
+++ src/KinkLibrary/useFilterKinks.ts
@@ -18,11 +18,52 @@ interface Filters {
 }
 
 export const useFilterKinks = (allKinks: Kinks) => {
+  const sortKinks = useCallback((kinks: Kinks) => {
+    // After filtering, sort so favorites always come first
+    return kinks.sort((a, b) => {
+      // If both or neither are favorite, they remain in their original order
+      if (a.favorite === b.favorite) return 0;
+      // If 'a' is favorite but 'b' is not, 'a' should come first
+      if (a.favorite && !b.favorite) return -1;
+      // If 'b' is favorite but 'a' is not, 'b' should come first
+      return 1;
+    });
+  }, [])
+
+  const onFilterChange = useCallback((filters: Filters) => {
+    const filtered = allKinks.filter((kink) => {
+      const matchesSearchText = filters.searchText === '' || kink.idea_title.toLowerCase().includes(filters.searchText.toLowerCase()) || kink.idea_description.toLowerCase().includes(filters.searchText.toLowerCase());
+
+      const matchesCategories = filters.selectedCategories.length === 0 || kink.categories.some(category => filters.selectedCategories.includes(category));
+
+      const matchesFavorite = !filters.isFavorite || kink.favorite;
+
+      const matchesStatus = filters.status === '' || kink.status === filters.status;
+
+      const matchesLevel = filters.level === '' || kink.level === filters.level;
+
+      return matchesSearchText && matchesCategories && matchesFavorite && matchesStatus && matchesLevel;
+    });
+
+    const sortedFiltered = sortKinks(filtered);
+
+    console.log({
+      sortedFiltered
+    })
+
+    setFilteredKinks(sortedFiltered);
+  }, [allKinks, sortKinks]);
+
   const [filteredKinks, setFilteredKinks] = useState<Kinks>([]);
 
   useEffect(() => {
-    setFilteredKinks(allKinks); // Initially, display all kinks
-  }, [allKinks]);
+    console.log('yo')
+    const sortedKinks = sortKinks(allKinks);
+    setFilteredKinks(sortedKinks);
+    console.log({
+      sortedKinks
+    })
+  }, [allKinks, sortKinks]);
 
 
   const extractCategories = (kinks: Kinks): CategoryWithCount[] => {
@@ -47,43 +88,12 @@ export const useFilterKinks = (allKinks: Kinks) => {
     return categoriesWithCounts;
   };
 
-
   const [categories, setCategories] = useState<CategoryWithCount[]>([]);
 
   useEffect(() => {
     const categoriesWithCounts = extractCategories(allKinks);
     setCategories(categoriesWithCounts);
   }, [allKinks])
-
-
-  const onFilterChange = useCallback((filters: Filters) => {
-    const filtered = allKinks.filter((kink) => {
-      const matchesSearchText = filters.searchText === '' || kink.idea_title.toLowerCase().includes(filters.searchText.toLowerCase()) || kink.idea_description.toLowerCase().includes(filters.searchText.toLowerCase());
-
-      const matchesCategories = filters.selectedCategories.length === 0 || kink.categories.some(category => filters.selectedCategories.includes(category));
-
-      const matchesFavorite = !filters.isFavorite || kink.favorite;
-
-      const matchesStatus = filters.status === '' || kink.status === filters.status;
-
-      const matchesLevel = filters.level === '' || kink.level === filters.level;
-
-      return matchesSearchText && matchesCategories && matchesFavorite && matchesStatus && matchesLevel;
-    });
-
-    // After filtering, sort so favorites always come first
-    const sortedFiltered = filtered.sort((a, b) => {
-      // If both or neither are favorite, they remain in their original order
-      if (a.favorite === b.favorite) return 0;
-      // If 'a' is favorite but 'b' is not, 'a' should come first
-      if (a.favorite && !b.favorite) return -1;
-      // If 'b' is favorite but 'a' is not, 'b' should come first
-      return 1;
-    });
-
-
-    setFilteredKinks(sortedFiltered);
-  }, [allKinks]);
 
   // Return both the filtered list of kinks and the function to change the filters
   return { filteredKinks, onFilterChange, categories };
