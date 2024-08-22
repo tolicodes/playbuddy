4fac40b3e0dab2e0c4677e8453b304aebdead4a2
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2024-08-22 11:04:43 -0400

    Fix App submission

--- kink-buddy-ios/App.tsx
+++ kink-buddy-ios/App.tsx
@@ -2,19 +2,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
+import IonIcon from 'react-native-vector-icons/Ionicons';
 
 
 import Web from './Web';
 import Calendar from './Calendar/Calendar'
 import { NavigationContainer } from '@react-navigation/native';
 import Resources from './Resources'
-import Feedback from './Feedback';
+import Moar from './Moar';
 
 type RootTabParamList = {
   Calendar: undefined;
   Kinks: undefined;
   Resources: undefined
-  Feedback: undefined
+  Moar: undefined
 };
 
 const Tab = createBottomTabNavigator<RootTabParamList>();
@@ -31,8 +32,8 @@ export default () => {
               return <MaterialIcon name="handcuffs" size={size} color={color} />;
             } else if (route.name === 'Resources') {
               return <FAIcon name="book" size={size} color={color} />;
-            } else if (route.name === "Feedback") {
-              return <FAIcon name="book" size={size} color={color} />;
+            } else if (route.name === "Moar") {
+              return <IonIcon name="ellipsis-horizontal" size={size} color={color} />;
             }
           },
         })}
@@ -44,7 +45,7 @@ export default () => {
         <Tab.Screen name="Calendar" component={Calendar} />
         <Tab.Screen name="Kinks" component={Web} />
         <Tab.Screen name="Resources" component={Resources} />
-        <Tab.Screen name="Feedback" component={Feedback} />
+        <Tab.Screen name="Moar" component={Moar} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- kink-buddy-ios/Moar.tsx
+++ kink-buddy-ios/Moar.tsx
@@ -1,15 +1,15 @@
-import { Text, SectionList, StyleSheet, Animated, Dimensions, SafeAreaView, TouchableOpacity, Linking } from 'react-native';
+import { Text, View, StyleSheet, Animated, Dimensions, SafeAreaView, TouchableOpacity, Linking } from 'react-native';
 import meta from '../src/KinkLibrary/FilterChips.stories';
 
 export default () => {
-    return (<SafeAreaView style={{ padding: 20 }}>
+    return (<View style={{ padding: 20 }}>
         <Text>Got feedback?</Text>
         <Text>Want to add your event? </Text>
 
         <Text>Contact me at</Text>
 
         <TouchableOpacity onPress={() => Linking.openURL("mailto:toli@toli.me")}>
-            <Text style={{ color: 'blue' }}> toli@toli.me</Text>
+            <Text style={{ color: 'blue' }}>toli@toli.me</Text>
         </TouchableOpacity>
-    </SafeAreaView>)
+    </View>)
 }
