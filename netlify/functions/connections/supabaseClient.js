105a955831ccc7d398dbc87a54b91357d393e0c6
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2024-08-27 12:00:42 -0400

    delete dist

--- netlify/functions/connections/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabaseUrl = 'https://eevykpzkermogzscjglo.supabase.co';
-const supabaseAdminKey = process.env.SUPABASE_ADMIN_KEY;
-
-const supabaseClient = createClient(supabaseUrl, supabaseAdminKey);
-
-export { supabaseClient }
