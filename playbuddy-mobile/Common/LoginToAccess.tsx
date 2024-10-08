b2fcbaa57b2b48f460d345ebf15d9b189bcbc7b6
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2024-10-08 14:57:52 -0400

    login to access component

---
+++ playbuddy-mobile/Common/LoginToAccess.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, } from "react-native";
+import HeaderLoginButton from "../Auth/HeaderLoginButton";
+
+export const LoginToAccess = ({ entityToAccess }: { entityToAccess: string }) => (
+    <View style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',  // Centers vertically
+        alignItems: 'center',      // Centers horizontally
+        height: '100%',            // Ensure the height takes full screen
+    }}>
+        <Text style={{ textAlign: 'center', marginTop: 10, fontSize: 20 }}>
+            Login to access {entityToAccess}
+        </Text>
+        <View style={{ marginTop: 10, }}>
+            <HeaderLoginButton showLoginText={true} />
+        </View>
+    </View>
+)
