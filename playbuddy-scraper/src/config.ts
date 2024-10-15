7cf2993ee1e33f81f4377f4ad2c14483723fca3b
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2024-10-15 17:26:32 -0400

    local api endpoint flag

--- playbuddy-scraper/src/config.ts
+++ playbuddy-scraper/src/config.ts
@@ -1,6 +1,10 @@
 // only edit /common/config.ts 
 // These are copied
-export const API_BASE_URL = 'https://api.playbuddy.me'
+
+export const API_BASE_URL_PROD = 'https://api.playbuddy.me'
+
+export const API_BASE_URL_LOCAL = 'http://localhost:8080'
+export const API_BASE_URL = process.env.NODE_ENV === 'production' ? API_BASE_URL_PROD : API_BASE_URL_LOCAL;
 
 export const API_URL = {
     events: `${API_BASE_URL}/events`,
