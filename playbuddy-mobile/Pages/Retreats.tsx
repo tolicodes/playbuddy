370e5f263482c8cd966dfd7af6e60ffc5f402844
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2024-10-18 15:40:37 -0400

    retreats banenr

--- playbuddy-mobile/Pages/Retreats.tsx
+++ playbuddy-mobile/Pages/Retreats.tsx
@@ -1,8 +1,42 @@
 import React from "react"
 import EventCalendarView from "../Calendar/EventCalendarView"
 
+import { View, Text, StyleSheet } from 'react-native';
+
+const Banner = () => {
+    return (
+
+        <View style={styles.banner}>
+            <Text style={styles.text}>
+                Please select "ALL" for Communities and Locations to view ALL retreats.
+            </Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    banner: {
+        backgroundColor: '#FF4D4D', // Red color
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: '#fff',
+        fontSize: 16,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+});
+
+export default Banner;
+
+
 export const Retreats = () => {
     return (
-        <EventCalendarView isRetreats={true} />
+        <>
+            <Banner />
+            <EventCalendarView isRetreats={true} />
+        </>
     )
 }
