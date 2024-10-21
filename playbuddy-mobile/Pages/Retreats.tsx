247e21648be0d129d78f8d1487989c9eb8bba086
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2024-10-21 15:01:08 -0400

    Retreats banner

--- playbuddy-mobile/Pages/Retreats.tsx
+++ playbuddy-mobile/Pages/Retreats.tsx
@@ -1,16 +1,52 @@
-import React from "react"
+import React, { useState } from "react"
 import EventCalendarView from "../Calendar/EventCalendarView"
 
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Linking, TouchableOpacity } from 'react-native';
+import { Community, LocationArea, useCommonContext } from "../Common/CommonContext";
 
 const Banner = () => {
-    return (
+    const [isBannerPressed, setIsBannerPressed] = useState(false);
+    const [oldLocation, setOldLocation] = useState<LocationArea | null>(null);
+    const [oldCommunity, setOldCommunity] = useState<Community | null>(null);
+    const { selectedLocationArea, selectedCommunity, setSelectedLocationArea, setSelectedCommunity } = useCommonContext();
 
-        <View style={styles.banner}>
-            <Text style={styles.text}>
-                Please select "ALL" for Communities and Locations to view ALL retreats.
-            </Text>
-        </View>
+    const handleBannerPress = () => {
+        if (isBannerPressed) {
+            setIsBannerPressed(false);
+            setSelectedLocationArea(oldLocation);
+            setSelectedCommunity(oldCommunity);
+            return;
+        }
+
+        setIsBannerPressed(true);
+        setOldLocation(selectedLocationArea || null);
+        setOldCommunity(selectedCommunity || null);
+        setSelectedLocationArea(null);
+        setSelectedCommunity(null);
+    }
+
+    return (
+        <>
+            <TouchableOpacity
+                style={isBannerPressed ? styles.bannerPressed : styles.banner}
+                onPress={handleBannerPress}
+            >
+                {isBannerPressed ? (
+                    <Text style={styles.bannerTextBold}>
+                        Now showing retreats from all communities worldwide. Filter on the top right.
+                    </Text>
+                ) : (
+                    <Text style={styles.bannerText}>🌎 See Retreats Worldwide 🌎</Text>
+                )}
+            </TouchableOpacity>
+            {isBannerPressed && (
+                <TouchableOpacity style={styles.banner} onPress={handleBannerPress}>
+                    <Text style={styles.bannerText}>
+                        Click to reset to &quot;{oldLocation?.name}&quot; and &quot;{oldCommunity?.name}&quot;.
+                    </Text>
+                </TouchableOpacity>
+            )}
+        </>
     );
 };
 
@@ -21,11 +57,23 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    text: {
+    bannerPressed: {
+        // ios blue color
+        backgroundColor: '#007AFF',
+        padding: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bannerText: {
         color: '#fff',
         fontSize: 16,
         textAlign: 'center',
         fontWeight: 'bold',
+    },
+    bannerTextBold: {
+        color: '#fff',
+        fontSize: 16,
+        textAlign: 'center',
     },
 });
 
