01b5f7ab70819210b315f7bd0b36fd7c7a1ae1c8
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2024-11-13 12:45:52 -0700

    redis cache locations

--- playbuddy-api/src/routes/personalization.ts
+++ playbuddy-api/src/routes/personalization.ts
@@ -1,19 +1,25 @@
 import { AuthenticatedRequest } from "middleware/authenticateRequest.js";
 import { supabaseClient } from "connections/supabaseClient.js";
 import { Request, Response, Router } from 'express';
-
+import { connectRedisClient } from '../connections/redisClient.js';
+import { fetchAndCacheData } from '../helpers/cacheHelper.js';
 
 export const fetchLocationAreas = async (req: AuthenticatedRequest, res: Response) => {
-    const { data, error } = await supabaseClient
-        .from('location_areas')
-        .select('id, name, code')
-        .order('name');
+    const cacheKey = 'locationAreas';
+    const redisClient = await connectRedisClient();
+    const responseData = await fetchAndCacheData(redisClient, cacheKey, async () => {
+        const { data, error } = await supabaseClient
+            .from('location_areas')
+            .select('id, name, code')
+            .order('name');
 
-    if (error) {
-        throw new Error(`Error fetching location areas: ${error.message}`);
-    }
+        if (error) {
+            throw new Error(`Error fetching location areas: ${error.message}`);
+        }
+        return data;
+    });
 
-    return res.json(data);
+    return res.json(responseData);
 }
 
 const router = Router();
