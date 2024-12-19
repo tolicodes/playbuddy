b60f8fc358436e1696fb155b5c8f74ef95993c0e
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2024-12-19 16:39:35 -0500

    Added promo to swipe

--- playbuddy-mobile/Pages/SwipeMode.tsx
+++ playbuddy-mobile/Pages/SwipeMode.tsx
@@ -4,7 +4,7 @@ import Swiper from 'react-native-deck-swiper';
 import { Image } from 'expo-image'
 import { useCalendarContext } from './Calendar/hooks/CalendarContext';
 import { formatDate } from './Calendar/hooks/calendarUtils';
-import { Event } from '../commonTypes';
+import { Event, PromoCode } from '../commonTypes';
 import moment from 'moment';
 import { useRecordSwipeChoice } from './Calendar/hooks/useWishlist';
 import { useUserContext } from './Auth/hooks/UserContext';
@@ -13,6 +13,7 @@ import { Button } from '@rneui/themed';
 import { getSmallAvatarUrl } from '../Common/hooks/imageUtils';
 import { logEvent } from '../Common/hooks/logger';
 import { useBadgeNotifications } from '../Common/Nav/useBadgeNotifications';
+import { addOrReplacePromoCode } from './Auth/screens/usePromoCode';
 
 export const SwipeMode = () => {
     const { availableCardsToSwipe } = useCalendarContext();
@@ -62,8 +63,32 @@ export const SwipeMode = () => {
             )
         }
 
+        const eventPromoCode = event.promo_codes?.find(code => code.scope === 'event');
+        const organizerPromoCode = event.organizer?.promo_codes?.find(code => code.scope === 'organizer');
+
+        const promoCode = eventPromoCode || organizerPromoCode;
+
+        const PromoCode = promoCode && (
+            <View style={styles.promoCodeBadge}>
+                <Text style={styles.promoCodeText}>
+                    Promo Code: {promoCode.promo_code}&nbsp;
+
+                </Text>
+                <Text style={styles.promoCodeText}>
+                    {promoCode.discount_type === 'percent' ? '' : '$'}
+                    {promoCode.discount}{promoCode.discount_type === 'percent' ? '%' : ''} off
+                </Text>
+            </View>
+        )
+
         // TODO: find out why event is not always available
         const imageUrl = event?.image_url && getSmallAvatarUrl(event?.image_url);
+
+        const addPromoCodeToUrlAndOpen = (eventUrl: string, promoCode: PromoCode | undefined) => {
+            const eventUrlWithPromoCode = addOrReplacePromoCode(eventUrl, promoCode?.promo_code);
+            Linking.openURL(eventUrlWithPromoCode || event.event_url)
+        }
+
         return (
             <View style={styles.card}>
                 <Image source={{ uri: imageUrl }} style={styles.image} />
@@ -71,14 +96,28 @@ export const SwipeMode = () => {
                     <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{moment(event?.start_date).format('dddd')}</Text>
                     <Text >{moment(event?.start_date).format('MMM D')} {formatDate(event)}</Text>
 
+                    {PromoCode}
+
+                    {event.visibility === 'private' && (
+                        <View style={styles.privateBadge}>
+                            <Text style={styles.privateBadgeText}>Private</Text>
+                        </View>
+                    )}
+
                     <Text style={styles.eventName}>{event.name}</Text>
 
                     <Text style={styles.organizer}>{event?.organizer?.name}</Text>
 
+                    {/* {event.sharedBuddies && (
+                        <View style={styles.buddyAvatarCarouselContainer}>
+                            <BuddyAvatarCarousel buddies={event.sharedBuddies} />
+                        </View>
+                    )} */}
+
                     <Button
-                        title="More Info"
+                        title="Get Tickets"
                         onPress={() => {
-                            Linking.openURL(event.event_url);
+                            addPromoCodeToUrlAndOpen(event.event_url, promoCode);
                             logEvent('swipe_mode_more_info_click', { eventId: event.id });
                         }}
 
@@ -101,6 +140,7 @@ export const SwipeMode = () => {
     if (!availableCardsToSwipe.length) {
         return <NoEventsToSwipe />
     }
+
 
     return (
         <View style={styles.container}>
@@ -154,7 +194,7 @@ const styles = StyleSheet.create({
     },
     card: {
         width: '100%',
-        height: 500,
+        height: 600,
         borderRadius: 10,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 6 },
@@ -188,5 +228,34 @@ const styles = StyleSheet.create({
         fontSize: 16,
         color: '#555',
         marginBottom: 20,
+    },
+    promoCodeBadge: {
+        backgroundColor: '#FFD700',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 12,
+        marginVertical: 10
+    },
+    promoCodeText: {
+        fontSize: 12,
+        color: 'black',
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    privateBadge: {
+        backgroundColor: '#FF6B6B',
+        paddingHorizontal: 8,
+        paddingVertical: 2,
+        borderRadius: 12,
+        marginLeft: 8,
+    },
+    privateBadgeText: {
+        color: 'white',
+        fontSize: 12,
+        fontWeight: 'bold',
+    },
+    buddyAvatarCarouselContainer: {
+        flex: 1,
+        marginLeft: 10
     },
 });
