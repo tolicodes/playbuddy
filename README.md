10523d3b3d9290800a56ce1d4a4eae8920efc663
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2025-04-03 11:41:01 -0400

    New table docs

--- README.md
+++ README.md
@@ -24,3 +24,81 @@ Configure static hosting for playbuddy.me
 
 1. Cloud Run > Domain Mappings > Add Mapping > playbuddy-api > Cloud Run Domain Mappings > Verify New Domain > View in Search Console > Domain > Add TXT Record > Go back to Mapping > Refresh > Select Domain > Subdomain (api) > Done
 2. Same for playbuddy-scraper
+
+## User Actions
+
+### Events
+
+| event_detail_get_tickets_clicked |
+| event_detail_google_calendar_clicked |
+| event_detail_link_clicked |
+| event_detail_organizer_clicked |
+| event_list_item_clicked |
+
+### Wishlist
+
+| event_list_item_wishlist_toggled |
+| my_calendar_share_wishlist_click |
+| swipe_mode_more_info_click |
+| swipe_mode_swipe_left |
+| swipe_mode_swipe_right |
+
+### Calendar View
+
+| calendar_day_clicked |
+| event_calendar_view_on_press_calendar |
+| event_calendar_view_on_press_day |
+| event_calendar_view_on_press_private_events |
+| header_back_button_clicked |
+| header_drawer_button_clicked |
+| header_filter_button_clicked |
+
+### Promo Codes
+
+| deep_link |
+| deep_link_params_set |
+| initial_deep_link |
+
+### Buddies
+
+| buddy_avatar_carousel_press |
+
+### Communities
+
+| community_events_community_joined |
+| community_list_community_joined |
+| community_list_community_left |
+| community_list_navigate_to_community_events |
+| community_list_navigate_to_join_community_button_pressed |
+
+### Auth
+
+| login_banner_clicked |
+| login_button_clicked |
+| personalization_modal_confirmed |
+| welcome_screen_register_clicked |
+| welcome_screen_skipped |
+| avatar_press_pick_image |
+| avatar_upload_completed |
+| avatar_upload_failed |
+| avatar_upload_started |
+
+### Defaults (ex: community, location)
+
+| default_community_selected_acro |
+| defaults_menu_community_item_selected |
+| defaults_menu_location_item_selected |
+
+### Filter (deprecated)
+
+| filter_done_button_clicked |
+| filter_organizers |
+| filter_organizers_reset |
+| filter_reset_button_clicked |
+
+### Moar Screen
+
+| moar_get_add_your_events_link |
+| moar_get_google_cal_link |
+| moar_get_in_touch_click_email |
+| moar_link_clicked |
--- playbuddy-scraper/src/commonTypes.ts
+++ playbuddy-scraper/src/commonTypes.ts
@@ -14,6 +14,11 @@ export type LocationArea = {
     id: string;
     name: string;
     code: string;
+    city: string;
+    region: string;
+    country: string;
+    aliases: string[];
+    entity_type: string;
 }
 
 export type Community = {
@@ -39,70 +44,177 @@ export interface PromoCode {
 
 export interface Event extends SourceMetadata {
     id: number;
+    /**
+     * The original id from the source (Eventbrite, Plura, etc)
+     */
     original_id?: string;
+    /**
+     * The name of the event
+     */
     name: string;
+    /**
+     * The start date of the event
+     */
     start_date: string;
+    /**
+     * The end date of the event
+     */
     end_date: string;
+    /**
+     * The url to buy the ticket
+     */
     ticket_url: string;
+    /**
+     * The url of the image/thumbnail/cover image
+     */
     image_url: string;
+    /**
+     * The url of the video (if any)
+     */
     video_url?: string;
+    /**
+     * The url of the event (usually same as the ticket_url)
+     */
     event_url: string;
+    /**
+     * The location/address of the event
+     */
     location: string;
+    /**
+     * The price of the event (freeform text)
+     */
     price: string;
+    /**
+     * The description of the event, markdown formatted
+     */
     description: string;
+    /**
+     * The tags of the event
+     */
     tags: string[];
+    /**
+     * The type of the event (event or retreat)
+     */
     type: 'event' | 'retreat'
+    /**
+     * The recurring type of the event (none, weekly, monthly)
+     */
     recurring: 'none' | 'weekly' | 'monthly'
 
-    // for the map
+    /**
+     * The latitude of the event
+     */
     lat?: number;
+    /**
+     * The longitude of the event
+     */
     lon?: number;
 
-    // these reference other tables
-    // organizer_id
+    /**
+     * the organizer record (joined on organizer_id)
+     */
 
     organizer: Organizer;
-    // location_area_id
-
+    /**
+     * The location area the event belongs to (joined on location_area_id)
+     * A location area is a city, region, country, etc
+     */
     location_area?: LocationArea;
-    // community_ids (many to many via join table event_communities)
+    /**
+     * The communities the event belongs to (many to many via join table event_communities)
+     * For example, a community could be "Acro" or "Conscious Touch"
+     */
     communities?: Community[];
+    /**
+     * The visibility of the event (public or private)
+     * You must be a member of the community to see the event if private
+     */
     visibility?: 'public' | 'private';
+
+    /**
+     * The promo codes for the event
+     * May be event specific or organizer specific
+     */
+    promo_codes?: PromoCode[];
+
+}
+
+/**
+ * Metadata about the source of the event
+ * used to override the default values when importing from a source
+ * For example, the dataset may be applied to all events from a source
+ */
+export interface SourceMetadata {
+    /**
+     * Where the event was scraped from
+     */
+    source_url?: string;
+    /**
+     * The timestamp the event was scraped
+     */
+    timestamp_scraped?: string;
+    /**
+     * The dataset the event was scraped from
+     * Group of events that are from the same source with common logic
+     */
+    dataset?: "Kink" | "Whatsapp POC" | 'Acro' | 'Conscious Touch';
+
+    /**
+     * The platform the event was scraped from
+     * For example, WhatsApp, Eventbrite, Plura, etc
+     */
+    source_origination_platform?: "WhatsApp" | "organizer_api" | 'acrofestivals' | 'facebook' | 'lu.ma';
+
+    /**
+     * The ticketing platform the event is sold on
+     * For example, Eventbrite, Plura, Partiful, lu.ma, etc
+     */
+    source_ticketing_platform?: "Eventbrite" | "Plura" | "Partiful" | "lu.ma";
+
+    /**
+     * The group id the event belongs to
+     * Usually for WhatsApp groups
+     */
+    source_origination_group_id?: string;
+    /**
+     * The group name the event belongs to
+     * Usually for WhatsApp groups
+     */
+    source_origination_group_name?: string;
+
+
+
+    /**
+     * CREATE NEW RECORD IMPORTS
+     * These are used to create other entities when importing from a source
+     * For example we can create a new organizer or specify an existing one
+     */
+
+    /**
+     * The communities the event belongs to
+     * For example, the communities for a certain WhatsApp group (Youtopia or Conscious Touch)
+     */
+    communities?: {
+        id: string;
+    }[];
+
+    type?: 'event' | 'retreat'
 
     promo_codes?: PromoCode[];
 
 }
 
-export interface SourceMetadata {
-    source_url?: string;
-    timestamp_scraped?: string;
-    dataset?: "Kink" | "Whatsapp POC" | 'Acro' | 'Conscious Touch';
-
-    // where it originated from
-    source_origination_platform?: "WhatsApp" | "organizer_api" | 'acrofestivals' | 'facebook' | 'lu.ma';
-
-    // the ticketing platform it's sold on
-    source_ticketing_platform?: "Eventbrite" | "Plura" | "Partiful" | "lu.ma";
-
-    // group id
-    source_origination_group_id?: string;
-    // group name
-    source_origination_group_name?: string;
-
-
-    // Fields
-    communities?: {
-        id: string;
-    }[];
-
+// Metadata for a list of events
+interface EventMetadata extends SourceMetadata {
+    id: string;
 }
+
+export type EventsMetadata = EventMetadata[];
 
 
 // either pass an id or inputs to create a new one
 export type CreateOrganizerInput = { original_id?: string; name: string; url: string } | { id: string }
 
-
-// we could omit a location name and it will use the code instead
 
 export type CreateLocationAreaInput = { code: string; name?: string } | { id: string }
 export type CreateCommunityInput = { name?: string; code: string } | { id: string }
@@ -116,3 +228,48 @@ export type CreateEventInput = Omit<Event,
     communities?: CreateCommunityInput[];
     metadata?: any;
 } & SourceMetadata
+
+
+/**
+ * TABLES
+ * These are the tables in the database
+ * 
+ * MAIN
+ * - events - events
+ * - organizers - organizers, linked to events
+ * - location_areas - areas where events are held - for example NYC, Spain
+ *
+ * USERS
+ * - users - list of users
+ * 
+ * WISHLISTS
+ * - event_wishlists - a list of events a user has added to their wishlist/my calendar
+ * - swipe_mode_choices - a list of events a user has swiped on (liked or not)
+ * 
+ * PROMO CODES
+ * - promo_codes - promo codes for events. Can be event specific or organizer specific
+ * - promo_code_events - links promo codes to events
+ * 
+ *  COMMUNITIES
+ * - communities - list of communities. Communities a public community (for an organizer) or a private community (for example a private WhatsApp group), braod interest group (acro or Kink)
+ * - community_curators - admins of a community
+ * - community_memberships - if you follow or a join a community
+ * - event_communities - is an event a part of a community (ex: Acro or a certain organizer)
+
+ * 
+ * BUDDIES
+ * - buddies - friends of the user
+ * - buddy_lists - lists of buddies. A user can have many buddy lists (friends, rope bunnies, poly circles, etc)
+ * - buddy_list_buddies - many to many relationship between buddy_lists and buddies
+ * 
+ * TRACKING
+ * - user_events - tracks all clicks and actions a user has taken on an event
+ * 
+ * ML
+ *  - classifications - metadata on event. Tags to search by. For example, comfort level (beginner, intermediate, advanced), accessibility
+ * 
+ * OTHER
+ * - kinks: kink database
+ * 
+ * 
+*/
