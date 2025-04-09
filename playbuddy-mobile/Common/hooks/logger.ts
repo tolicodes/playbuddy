83879ef15088a2a7ba6946e7aab4ac989fb7bb68
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2025-04-09 12:41:21 -0400

    New logger

--- playbuddy-mobile/Common/hooks/logger.ts
+++ playbuddy-mobile/Common/hooks/logger.ts
@@ -2,18 +2,22 @@ import * as amplitude from '@amplitude/analytics-react-native';
 import RNUxcam from 'react-native-ux-cam';
 import axios from 'axios';
 import { API_BASE_URL } from '../../config';
+import { UserEventName, UserEventInput } from '../../../common/types/userEventTypes';
 
-export const logEvent = (event: string, data?: any) => {
+export function logEvent(
+    name: UserEventName,
+    props: unknown
+) {
     try {
-        amplitude.logEvent(event, data);
-        RNUxcam.logEvent(event, data);
-        logUserEvent({ user_event_name: event, user_event_props: data });
+        amplitude.logEvent(name, props || {});
+        RNUxcam.logEvent(name, props || {});
+        logUserEventToDB({ user_event_name: name, user_event_props: props });
     } catch (error) {
         // this means it's in dev probably which doesn't support uxcam
     }
 }
 
-const logUserEvent = async ({ user_event_name, user_event_props }: { user_event_name: string, user_event_props?: any }) => {
+const logUserEventToDB = async ({ user_event_name, user_event_props }: { user_event_name: string, user_event_props?: any }) => {
     try {
         const { data } = await axios.post(`${API_BASE_URL}/user_events`, { user_event_name, user_event_props });
         return data;
