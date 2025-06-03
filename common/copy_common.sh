c9c8a6e7a022bf2d38b7caf15ce4f8dbd12428d8
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2025-06-03 15:39:49 -0400

    reorganized copy common

--- common/copy_common.sh
+++ common/copy_common.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+
+# OLD
+
 # Define the source files
-SOURCE_FILES=("common/types/commonTypes.ts" "common/types/userEventTypes.ts" "common/config.ts")
+SOURCE_FILES=(
+  "common/src/types/commonTypes.ts" 
+  "common/src/types/userEventTypes.ts" 
+  "common/src/config.ts"
+)
 
 # Define the target directories
 TARGET_DIRECTORIES=(
@@ -15,12 +22,41 @@ TARGET_DIRECTORIES=(
 for TARGET_DIR in "${TARGET_DIRECTORIES[@]}"; do
   if [ -d "$TARGET_DIR" ]; then
     for SOURCE_FILE in "${SOURCE_FILES[@]}"; do
-      cp "$SOURCE_FILE" "$TARGET_DIR/"
+      cp "$SOURCE_FILE" "$TARGET_DIR"
       echo "Copied $SOURCE_FILE to $TARGET_DIR/"
     done
   else
     echo "Target directory $TARGET_DIR does not exist. Skipping..."
   fi
 done
+
+# END OLD
+
+
+# NEW!!
+
+SOURCE_DIR="common/src"
+
+# Define the target directories
+TARGET_DIRECTORIES=(
+  # "playbuddy-mobile"
+  # "playbuddy-scraper/src"
+  # "playbuddy-api/src"
+  # "playbuddy-web/src"
+  "playbuddy-admin/src"
+)
+
+# Copy the common directory to each target directory
+for TARGET_DIR in "${TARGET_DIRECTORIES[@]}"; do
+  if [ -d "$TARGET_DIR" ]; then
+    cp -R "$SOURCE_DIR" "$TARGET_DIR/common"
+    echo "Copied common directory to $TARGET_DIR/"
+  else
+    echo "Target directory $TARGET_DIR does not exist. Skipping..."
+  fi
+done
+
+# END NEW!!
+
 
 echo "Done!"
