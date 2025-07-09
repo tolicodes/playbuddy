91122522b721c19977172ff9254dec4713fb2903
Anatoliy (Toli) Zaslavskiy <toli@tolicodes.com>
2025-07-09 17:45:59 -0400

    chrome scraper types

---
+++ playbuddy-chrome-scraper/src/types.ts
@@ -0,0 +1,16 @@
+export interface EventResult {
+    name?: string | null;
+    rawDatetime?: string | null;
+    location?: string | null;
+    category?: string | null;
+    description?: string | null;
+    description_html?: string | null;
+    ticket_url?: string | null;
+    fetlife_handle?: string | null;
+    instagram_username?: string | null;
+    organizer_href?: string | null;
+    start_date?: string | null;
+    end_date?: string | null;
+    image?: string | null;
+    error?: string;
+}
